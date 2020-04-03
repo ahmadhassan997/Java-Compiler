0397d4478f8a7dbbe0948c0b60f2fa4e0ed7d5fb
ahmadhassan997 <riemann997@gmail.com>
2020-04-03 10:24:12 +0500

    lexer code complete now

---
+++ lexer/Real.java
@@ -0,0 +1,7 @@
+package lexer;
+
+public class Real extends Token {
+    public final float value;
+    public Real(float value) {super(Tag.REAL); this.value = value;}
+    public String toString() {return "" + value;}
+}

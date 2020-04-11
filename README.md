e30b248aa18cdc63afdc966d9031f905534b512b
ahmadhassan997 <riemann997@gmail.com>
2020-04-11 18:54:37 +0500

    test files + compile instructions added

--- README.md
+++ README.md
@@ -2,6 +2,34 @@
 
 Compiler Implementation in Java.
 
-Details will be added later in README.
+Each package/folder have its own README file.
 
-Each package/folder will have its own README file.
+## Compile and Test
+
+**Compile** this code with following commands:
+
+``` bash
+javac lexer/*.java
+```
+
+``` bash
+javac parser/*.java
+```
+
+``` bash
+javac inter/*.java
+```
+
+``` bash
+javac symbols/*.java
+```
+
+``` bash
+javac main/*.java
+```
+
+To **test** the code:
+
+``` bash
+java main.Main __inputfile__ __outputfile__
+```
--- main/Main.java
+++ main/Main.java
@@ -5,6 +5,25 @@ import lexer.*; import parser.*;
 
 public class Main {
     public static void main(String []args) throws IOException {
+
+        // for input stream : optional
+        if ( args.length >= 1) {
+            try {
+                FileInputStream inFile = new FileInputStream(args[0]);
+                System.setIn(inFile);
+            }
+            catch (FileNotFoundException err) {
+                err.printStackTrace();
+            }
+        }
+
+        // for output stream : optional
+        if ( args.length >= 2) {
+            PrintStream outFile = new PrintStream(new FileOutputStream(args[1]));
+            System.setOut(outFile);
+        }
+
+        // Initialize front-end syntax directed translator
         Lexer lex = new Lexer();
         Parser parse = new Parser(lex);
         parse.program();
